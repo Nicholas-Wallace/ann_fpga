1994b7fed52ce276a003593cd0c94d410e8c87b2
Nicholas-Wallace <nicholaswlopes@hotmail.com>
2025-07-04 16:41:41 -0300

    Update controlador.vhd

--- controlador.vhd
+++ controlador.vhd
@@ -4,7 +4,7 @@ USE ieee.std_logic_arith.all;
 
 entity controlador is
 	port(
-		en : in std_logic;
+		en_contr : in std_logic;
 		clock : in std_logic;
 		
 		-----------------------------------------
@@ -89,7 +89,7 @@ begin
 	
 	state <= S_SIGNAL;
 	
-	C : combinacional port map(en_comb => en, S => S_SIGNAL, NS => NS_SIGNAL, add_W0_comb => add_W0, add_W1_comb => add_W1, add_W2_comb => add_W2, add_W3_comb => add_W3,
+	C : combinacional port map(en_comb => en_contr, S => S_SIGNAL, NS => NS_SIGNAL, add_W0_comb => add_W0, add_W1_comb => add_W1, add_W2_comb => add_W2, add_W3_comb => add_W3,
 																		 add_B0_comb => add_B0, add_B1_comb => add_B1, add_B2_comb => add_B2, add_B3_comb => add_B3,
 																		 en_ROM_B_comb => en_ROM_B, en_ROM_W_comb => en_ROM_W,
 																		 en_n00_comb => en_n00,
